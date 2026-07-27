d9c8cabcad57af29e9c282bb6aa547140bfa5d48
andrewstane451-cmd <andrewstane451@gmail.com>
2026-07-27 15:04:53 +0300

    Update route.ts

--- app/api/send-sm/route.ts
+++ app/api/send-sm/route.ts
@@ -1,1 +1,22 @@
+import { NextResponse } from "next/server";
 
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const response = await fetch(
+    "https://app.mobitechtechnologies.com/sms/sendsms",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        h_api_key:
+          "1f4e9fd54cb1dae36b53b5ec9505333dbcf7f2267d44c323125325f304ad9b17",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+
+  const data = await response.json();
+
+  return NextResponse.json(data);
+}
